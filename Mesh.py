1ee7598414a10d342216683f11e6d1b94091bb0f
Frederico Dias Paulino Da Costa <fd14866@bristol.ac.uk>
2023-03-30 16:01:30 +0100

    style: clean & comment
    clean and comment Mesh class object

--- Mesh.py
+++ Mesh.py
@@ -1,84 +1,21 @@
+# Import external libraries
 import pyvista as pv
 import numpy as np
 import pdb
 import vtk
 import os
 
-import DataManager
-from scipy.interpolate import Rbf
-
 class Mesh():
     self = None
-    
+# ------------------------------------------------------------------------ #
+# Class constructor
+# ------------------------------------------------------------------------ #    
     def __init__(self):
         self.boundBox = pv.Box()
         pass
-        
-    def construct_light_sensor(self, xMin, xMax, yMin, yMax, zMin, zMax):
-        self.grid = pv.Box(bounds=(xMin,xMax,yMin,yMax,zMin,zMax))
-  
-    def construct_unstructured_grid_from_dat(self, fileName):
-        cwd = os.getcwd()
-        os.chdir(cwd + "\processed_mesh")
-    
-        reader = DataManager.DataManager(fileName)
-        reader.open_and_read_dat()
-                 
-        # cellTypes       = np.empty(reader.numOfElements, dtype=np.uint8)
-        # cellTypes[:]    = vtk.VTK_TETRA
-        # self.cells      = np.array(reader.cells).ravel()
-        # self.cellTypes  = cellTypes
-        
-        self.cellTypes  = np.array(reader.cellTypes, dtype=np.uint8).ravel()
-        flatList        = [item for sublist in reader.cells for item in sublist]
-        self.cells      = np.array(flatList)
-        
-        
-        
-        # reader.open_and_read_mixed_ele_dat()
-        
-        self.points     = np.array(reader.points)
-        self.attributes = np.array(reader.attributes)
-        # flatList        = [item for sublist in reader.cells for item in sublist]
-        # self.cells      = np.array(flatList)
-        # self.cellTypes  = np.array(reader.cellTypes, dtype=np.uint8).ravel()
-        
-        
-        self.grid = pv.UnstructuredGrid(self.cells, self.cellTypes, self.points)
-        self.grid["Density"] = self.attributes
-        
-        # plotter = pv.Plotter()
-        # plotter.add_mesh_clip_box(self.grid, scalars='Density',  cmap='jet', show_edges=True)
-        # plotter.add_axes(color='black')
-        # plotter.set_background('white')
-        # plotter.show()
-        # pdb.set_trace()
-        
-        os.chdir(cwd)
-        
-    def construct_unstructured_grid_from_ply(self, fileName):
-        cwd = os.getcwd()
-        os.chdir(cwd + "\processed_mesh")
-        
-        pd = pv.read(fileName)
-
-        appendFilter = vtk.vtkAppendFilter()
-        appendFilter.AddInputData(pd)
-        appendFilter.Update()
-
-        uGrid = vtk.vtkUnstructuredGrid()
-        uGrid.ShallowCopy(appendFilter.GetOutput())
-
-        self.grid = pv.UnstructuredGrid(uGrid)
-        self.grid["Density"] = np.zeros((self.grid.points.shape[0],1))
-       
-        os.chdir(cwd)
-        
-    def build_kd_tree(self):
-        self.kdtree     = vtk.vtkKdTreePointLocator()
-        self.kdtree.SetDataSet(self.grid)
-        self.kdtree.BuildLocator()
-    
+# ------------------------------------------------------------------------ #
+# Tree data structures for point location
+# ------------------------------------------------------------------------ #        
     def build_obb_tree(self):
         geomFilter = vtk.vtkGeometryFilter()
         geomFilter.SetInputData(self.grid)
@@ -86,23 +23,31 @@ class Mesh():
         self.obbtree    = vtk.vtkOBBTree()
         self.obbtree.SetDataSet(geomFilter.GetOutput())
         self.obbtree.BuildLocator()
-    
+        
+    def build_kd_tree(self):
+        self.kdtree     = vtk.vtkKdTreePointLocator()
+        self.kdtree.SetDataSet(self.grid)
+        self.kdtree.BuildLocator() 
+    def build_point_locator(self):
+        self.pointLocator = vtk.vtkKdTreePointLocator()
+        self.pointLocator.SetDataSet(self.grid)
+        self.pointLocator.BuildLocator()       
     def build_octree(self):
+    
         self.octree     = vtk.vtkOctreePointLocator()
         self.octree.SetDataSet(self.grid)
         self.octree.BuildLocator()
-        
+# ------------------------------------------------------------------------ #
+# Tree data structure for cell location
+# ------------------------------------------------------------------------ #
     def build_cell_locator(self):
         # self.cellLocator = vtk.vtkCellLocator()
         self.cellLocator = vtk.vtkCellTreeLocator()
         self.cellLocator.SetDataSet(self.grid)
         self.cellLocator.BuildLocator()
-
-    def build_point_locator(self):
-        self.pointLocator = vtk.vtkKdTreePointLocator()
-        self.pointLocator.SetDataSet(self.grid)
-        self.pointLocator.BuildLocator()
-       
+# ------------------------------------------------------------------------ #
+# Filter for point in/out location
+# ------------------------------------------------------------------------ #
     def build_grid_in_out_checker(self, tol):
         geomFilter = vtk.vtkGeometryFilter()
         geomFilter.SetInputData(self.grid)
@@ -110,7 +55,7 @@ class Mesh():
         gridBoundary = geomFilter.GetOutput()
         self.inOutChecker = vtk.vtkSelectEnclosedPoints()
         self.inOutChecker.SetSurfaceData(gridBoundary)
-        self.inOutChecker.SetTolerance(tol)
+        self.inOutChecker.SetTolerance(tol)      
         
     def build_bbox_in_out_checker(self, tol):
         self.boundBox   = pv.Box(self.grid.bounds)
@@ -121,22 +66,9 @@ class Mesh():
         self.boundBox.inOutChecker = vtk.vtkSelectEnclosedPoints()
         self.boundBox.inOutChecker.SetSurfaceData(bBoxBoundary)
         self.boundBox.inOutChecker.SetTolerance(tol)    
-        # mapper = vtk.vtkPolyDataMapper()
-        # mapper.SetInputData(gridBoundary)
-        # actor = vtk.vtkActor()
-        # actor.SetMapper(mapper)
-        # actor.GetProperty().SetRepresentationToWireframe()
-        # renderer = vtk.vtkRenderer()
-        # renderer.AddActor(actor)
-        # renderer.SetBackground(1,1,1)
-        # window = vtk.vtkRenderWindow()
-        # window.AddRenderer(renderer)
-        # interactor = vtk.vtkRenderWindowInteractor()
-        # interactor.SetRenderWindow(window)
-        # window.Render()
-        # interactor.Start()
-        
-        # pdb.set_trace()
+# ------------------------------------------------------------------------ #
+# Filter and chosen kernel for point interpolation
+# ------------------------------------------------------------------------ #
     def set_gaussian_interpolator(self, sharpness, radius):
         gaussianKernel = vtk.vtkGaussianKernel()
         gaussianKernel.SetSharpness(sharpness)
@@ -145,8 +77,6 @@ class Mesh():
         self.interpolator.SetSourceData(self.grid)
         self.interpolator.SetKernel(gaussianKernel)
         self.interpolator.SetLocator(self.octree)
-        # self.interpolator.SetNullPointsStrategyToClosestPoint()
-        # self.interpolator.SetNullPointsStrategyToMaskPoints()
         self.interpolator.SetNullValue(0.0)
         self.interpolator.SetNullPointsStrategyToNullValue()
 
@@ -156,8 +86,6 @@ class Mesh():
         self.interpolator.SetSourceData(self.grid)
         self.interpolator.SetKernel(voronoiKernel)
         self.interpolator.SetLocator(self.octree)
-        # self.interpolator.SetNullPointsStrategyToClosestPoint()
-        # self.interpolator.SetNullPointsStrategyToMaskPoints()
         self.interpolator.SetNullValue(0.0)
         self.interpolator.SetNullPointsStrategyToNullValue()
 
@@ -169,8 +97,6 @@ class Mesh():
         self.interpolator.SetSourceData(self.grid)
         self.interpolator.SetKernel(shepardKernel)
         self.interpolator.SetLocator(self.octree)
-        # self.interpolator.SetNullPointsStrategyToClosestPoint()
-        # self.interpolator.SetNullPointsStrategyToMaskPoints()
         self.interpolator.SetNullValue(0.0)
         self.interpolator.SetNullPointsStrategyToNullValue()
 
@@ -181,8 +107,6 @@ class Mesh():
         self.interpolator.SetSourceData(self.grid)
         self.interpolator.SetKernel(linearKernel)
         self.interpolator.SetLocator(self.octree)
-        # self.interpolator.SetNullPointsStrategyToClosestPoint()
-        # self.interpolator.SetNullPointsStrategyToMaskPoints()
         self.interpolator.SetNullValue(0.0)
         self.interpolator.SetNullPointsStrategyToNullValue()
         
@@ -196,27 +120,28 @@ class Mesh():
         self.interpolator.SetSourceData(self.grid)
         self.interpolator.SetKernel(gaussianKernel)
         self.interpolator.SetLocator(self.octree)
-        # self.interpolator.SetNullPointsStrategyToClosestPoint()    
-        # self.interpolator.SetNullPointsStrategyToMaskPoints()  
         self.interpolator.SetNullValue(0.0)        
         self.interpolator.SetNullPointsStrategyToNullValue()        
   
+# ------------------------------------------------------------------------ #
+# Surface normal computation
+# ------------------------------------------------------------------------ #
     def compute_surface_normals(self):
         geomFilter = vtk.vtkGeometryFilter()
         geomFilter.SetInputData(self.grid)
         geomFilter.Update()
         
-        # normalGenerator = vtk.vtkPolyDataNormals()
         normalGenerator = vtk.vtkTriangleMeshPointNormals()
         normalGenerator.SetInputData(geomFilter.GetOutput())
-        # normalGenerator.ComputePointNormalsOn()
-        # normalGenerator.ComputeCellNormalsOff()
         normalGenerator.Update()
         self.grid['Normals'] = -np.array(normalGenerator.GetOutput().GetPointData().GetArray('Normals'))
         
+# ------------------------------------------------------------------------ #
+# Index-of-refraction computation
+# ------------------------------------------------------------------------ #        
     def compute_index_of_refraction(self, GDC):
-        self.grid['IOR']        = (1 + GDC*self.grid["Density"])
-        self.grid["IORSqrdVertex"]    = 0.5*(self.grid['IOR']**2)
+        self.grid['IOR']                = (1 + GDC*self.grid["Density"])
+        self.grid["IORSqrdVertex"]      = 0.5*(self.grid['IOR']**2)
         
         tmp = vtk.vtkPointDataToCellData()
         tmp.ProcessAllArraysOff()
@@ -226,7 +151,10 @@ class Mesh():
         tmp = pv.UnstructuredGrid(tmp.GetOutput())
         
         self.grid.cell_arrays['IORSqrdCell'] = tmp['IORSqrdVertex']
-
+        
+# ------------------------------------------------------------------------ #
+# Gradient vector computation
+# ------------------------------------------------------------------------ #        
     def compute_gradient(self):
         self.grid = self.grid.compute_derivative(scalars="IORSqrdVertex", gradient="Gradient", preference='point')
 
@@ -236,6 +164,9 @@ class Mesh():
     def compute_cell_gradient(self):
         self.grid = self.grid.compute_derivative(scalars="IORSqrdCell", gradient="GradientCell", faster=True, preference='cell')        
 
+# ------------------------------------------------------------------------ #
+# Point in/out location computation
+# ------------------------------------------------------------------------ #
     def locate_point_in_out_grid(self, p):
         self.inOutChecker.SetInputData(pv.PolyData(p))
         self.inOutChecker.Update()
@@ -247,8 +178,10 @@ class Mesh():
         self.boundBox.inOutChecker.Update()
         check = self.boundBox.inOutChecker.GetOutput().GetPointData().GetArray('SelectedPoints').GetTuple(0)[0]
         return check
-
-
+        
+# ------------------------------------------------------------------------ #
+# Scalar field interpolation via Hyperplane Fitting method
+# ------------------------------------------------------------------------ #
     def interpolate_density_field(self, interpPt):
         closestPoint  = [0,0,0]
         genCell       = vtk.vtkGenericCell()
